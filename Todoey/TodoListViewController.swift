2933274f6c1c5d2c0f4d858375808b0739ce48f5
conqsolid <conqsolid@gmail.com>
2018-08-30 16:52:48 +0300

    Space after mark

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -23,6 +23,7 @@ class TodoListViewController: UITableViewController {
     }
 
     //MARK - TableView Datasource Methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
